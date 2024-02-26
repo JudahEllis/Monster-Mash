ad29e908a62c051bc265a9b507bb0496c18b604e
JudahEllis <107877135+JudahEllis@users.noreply.github.com>
2024-02-25 21:36:56 -0800

    locomotion!
    it be a locomotion

--- Monster Mash/Monster Mash/Assets/Scripts/Controller2D.cs
+++ Monster Mash/Monster Mash/Assets/Scripts/Controller2D.cs
@@ -6,29 +6,40 @@ using TMPro;
 
 public class Controller2D : MonoBehaviour
 {
+    private bool facingRight = true;
     private Rigidbody2D rb;
-    [SerializeField] private float playerSpeed = 8.0f;
-    [SerializeField] private float playerRunSpeed = 9.33f;
-    [SerializeField] private float decelerate = 10f;
-    [SerializeField] private float jumpForce = 8.0f;
-    private float jumpTime = 1f;
+    private float playerSpeed = 12.0f;
+    private float playerRunSpeed;
+    private float playerAirSpeed = 11.0f;
+    private float playerAirBackwardSpeed = 4.0f;
+    private float decelerate = 1f;
+    private float jumpForce = 10f;
+    private float midAirJumpForce = 18f;
+    private float jumpCounter;
+    private float jumpTime = 0.2f;
+    private bool isJumping = false; //for grounded jump, longer hold = higher jump
+    private float jumpMultiplier = 14f;
+    private float fastFallSpeed = 3f;
+    private float airDrag;
+    private float fastAirDrag = 0.95f;
+    private float slowAirDrag = 0.65f;
     private int midAirJumps = 1;
     private int jumpsLeft = 0;
     private bool cannotJumpCuzMidJump = false;
-    [SerializeField] private bool grounded;
+    private bool grounded;
     private CapsuleCollider2D cap;
     private bool wall;
-    [SerializeField] private float move;
-    [SerializeField] private float fallMultiplier = 3f;
+    private int move;
+    private float fallMultiplier = 3f;
 
     private Vector3 xMovement;
     private Vector3 yMovement;
 
     [SerializeField] private LayerMask groundLayerMask;
 
-    [SerializeField] private bool isRun = false;
-    [SerializeField] private bool isSkid = false;
-    private float skidTime = 0.25f;
+    private bool isRun = false;
+    private bool isSkid = false;
+    private float skidTime = 0f;
 
 
     public KeyCode keyToDetect1 = KeyCode.A;
@@ -39,16 +50,10 @@ public class Controller2D : MonoBehaviour
     private bool key2WasPressed;
     private float timeSinceLastPress1;
     private float timeSinceLastPress2;
-    [SerializeField] private int key1result;
-    [SerializeField] private int key2result;
+    private int key1result;
+    private int key2result;
 
-    //ui
-    [SerializeField] Slider forceButton;
-    [SerializeField] TMP_Text forceText;
-    [SerializeField] Slider gravityButton;
-    [SerializeField] TMP_Text gravityText;
-    [SerializeField] Slider fallButton;
-    [SerializeField] TMP_Text fallText;
+    [SerializeField] private Transform playerNose;
 
     private void Start()
     {
@@ -57,26 +62,22 @@ public class Controller2D : MonoBehaviour
 
         playerRunSpeed = playerSpeed * (13f / 10f);
 
-        jumpForce = 21.6f;
-        rb.gravityScale = 6.28f;
+        //jumpForce = 21.6f;
+        rb.gravityScale = 4f;//6.28f;
         fallMultiplier = 4f;
-
-        forceButton.value = jumpForce;
-        gravityButton.value = rb.gravityScale;
-        fallButton.value = fallMultiplier;
     }
 
     private void Update()
     {
-        jumpForce = forceButton.value;
-        rb.gravityScale = gravityButton.value;
-        fallMultiplier = fallButton.value;
+        if (facingRight && playerNose != null)
+        {
+            playerNose.localPosition = new Vector2(0.5f, playerNose.localPosition.y);
+        } else if (playerNose != null)
+        {
+            playerNose.localPosition = new Vector2(-0.5f, playerNose.localPosition.y);
+        }
 
-        forceText.text = jumpForce.ToString();
-        gravityText.text = rb.gravityScale.ToString();
-        fallText.text = fallMultiplier.ToString();
-
-        if (Input.GetKeyDown(keyToDetect1))
+        if (Input.GetKeyDown(keyToDetect1) && !Input.GetKey(keyToDetect2))
         {
             if (key1WasPressed && Time.time - timeSinceLastPress1 < doubleTapTimeThreshold)
             {
@@ -97,7 +98,7 @@ public class Controller2D : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(keyToDetect2))
+        if (Input.GetKeyDown(keyToDetect2) && !Input.GetKey(keyToDetect1))
         {
             if (key2WasPressed && Time.time - timeSinceLastPress2 < doubleTapTimeThreshold)
             {
@@ -123,28 +124,90 @@ public class Controller2D : MonoBehaviour
             cannotJumpCuzMidJump = false;
         }
 
-        //MoveX();
-
-        //MoveY();
+        MoveX();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (grounded)
             {
+                if (isSkid)
+                {
+                    StopCoroutine("SkidToStop");
+                    isSkid = false;
+                }
+
+                if (move != 0)
+                {
+                    airDrag = fastAirDrag;
+                } else
+                {
+                    airDrag = slowAirDrag;
+                }
+
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
                 jumpsLeft = midAirJumps;
+                isJumping = true;
+                print("isJump true");
+                jumpCounter = 0;
+
             } else if (jumpsLeft > 0)
             {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                if (isSkid)
+                {
+                    StopCoroutine("SkidToStop");
+                    isSkid = false;
+                }
+
+                if (move != 0)
+                {
+                    airDrag = fastAirDrag;
+                }
+                else
+                {
+                    airDrag = slowAirDrag;
+                }
+
+                rb.velocity = new Vector2(rb.velocity.x, midAirJumpForce);
                 jumpsLeft--;
+
+                if (move == 1)
+                {
+                    facingRight = true;
+                } else if (move == -1)
+                {
+                    facingRight = false;
+                }
             }
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            jumpCounter += Time.deltaTime;
+            if (jumpCounter > jumpTime) { isJumping = false; print("isJump false"); }
+
+            float t = jumpCounter / jumpTime;
+
+            float currentJumpM = jumpMultiplier;
+
+            if (t > 0.5f)
+            {
+                currentJumpM = jumpMultiplier * (1 - t);
+            }
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpMultiplier);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && isJumping)
+        {
+            print("isJump false");
+            isJumping = false;
         }
 
         if (rb.velocity.y < 0.0f)
         {
             if (Input.GetAxis("Vertical") < 0)
             {
-                rb.velocity -= -Physics2D.gravity * fallMultiplier * 5 * Time.deltaTime;
+                rb.velocity -= -Physics2D.gravity * fallMultiplier * fastFallSpeed * Time.deltaTime;
             }
             else
             {
@@ -155,15 +218,6 @@ public class Controller2D : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Check for grounded state (you can use raycasting or other methods).
-        //IsGrounded();
-
-        // Jumping.
-        /*if (Input.GetKeyDown(KeyCode.Space))// && grounded)
-        {
-            //rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }*/
-
         IsGrounded();
 
         ApplyMove();
@@ -171,7 +225,6 @@ public class Controller2D : MonoBehaviour
 
     private void IsGrounded()
     {
-        //if (Physics2D.CapsuleCast(transform.position, cap.size * 0.75f, cap.direction, 0, Vector2.down, 0.1f, groundLayerMask))
         if (Physics2D.BoxCast(new Vector2(transform.position.x, cap.bounds.min.y), new Vector2(cap.bounds.size.x, 0.25f), 0, Vector2.down, 0.1f, groundLayerMask))
         {
             grounded = true;
@@ -194,47 +247,68 @@ public class Controller2D : MonoBehaviour
     private void MoveX()
     {
         // Horizontal movement.
-        move = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.D))
+        {
+            move = 1;
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            move = -1;
+        } else
+        {
+            move = 0;
+        }
 
         //disable move
         //move = 0;
 
         if (!HitWall(move))
         {
-            if ((key1result == 2 && move < 0) || (key2result == 2 && move > 0))//(Input.GetKey(KeyCode.LeftShift))
-            {
-                isRun = true;
-                xMovement = new Vector3(move * playerRunSpeed, 0.0f, 0.0f);
-            }
-            else
-            {
-                isRun = false;
-                xMovement = new Vector3(move * playerSpeed, 0.0f, 0.0f);
-            }
-        }
-    }
-
-    private void MoveY()
-    {
-
-        /*if (Input.GetKey(KeyCode.Space) && !cannotJumpCuzMidJump)
-        {
             if (grounded)
             {
-                yMovement = Vector3.up * jumpForce;
-                jumpsLeft = midAirJumps;
-                cannotJumpCuzMidJump = true;
-            }
-            else if (jumpsLeft > 0)
+                if ((key1result == 2 && move < 0) || (key2result == 2 && move > 0))
+                {
+                    isRun = true;
+                    xMovement = new Vector3(move * playerRunSpeed, 0.0f, 0.0f);
+                }
+                else
+                {
+                    isRun = false;
+                    xMovement = new Vector3(move * playerSpeed, 0.0f, 0.0f);
+                }
+            } else
             {
-                yMovement = Vector3.up * jumpForce;
-                jumpsLeft--;
-                cannotJumpCuzMidJump = true;
+                if (move == 0 && Mathf.Approximately(rb.velocity.x, 0.0f)) // Jumping in place
+                {
+                    airDrag = slowAirDrag;
+                }
+                else if (facingRight)
+                {
+                    if (move > 0)
+                    {
+                        xMovement = new Vector3(playerAirSpeed, 0.0f, 0.0f);
+                    } else if (move == 0)
+                    {
+                        xMovement = new Vector3(rb.velocity.x * airDrag, 0.0f, 0.0f);
+                    } else if (move < 0)
+                    {
+                        xMovement = new Vector3(-playerAirBackwardSpeed, 0.0f, 0.0f);
+                    }
+                } else
+                {
+                    if (move > 0)
+                    {
+                        xMovement = new Vector3(playerAirBackwardSpeed, 0.0f, 0.0f);
+                    }
+                    else if (move == 0)
+                    {
+                        xMovement = new Vector3(rb.velocity.x * airDrag, 0.0f, 0.0f);
+                    }
+                    else if (move < 0)
+                    {
+                        xMovement = new Vector3(-playerAirSpeed, 0.0f, 0.0f);
+                    }
+                }
             }
-        }*/
-        //else
-        {
-            yMovement = Vector3.zero;
         }
     }
 
@@ -242,26 +316,27 @@ public class Controller2D : MonoBehaviour
     {
         if (!isSkid) //x movement
         {
-            if ((isRun && (move < 0 && rb.velocity.x > 0) || (move > 0 && rb.velocity.x < 0)))
+            if (grounded)
             {
-                //skid
-                //rb.AddForce(new Vector2(-rb.velocity.x * decelerate, 0f), ForceMode2D.Force);
+                if (xMovement.x > 0)
+                {
+                    facingRight = true;
+                }
+                else if (xMovement.x < 0)
+                {
+                    facingRight = false;
+                }
+            }
+
+            if (grounded && (isRun && (move < 0 && rb.velocity.x > 0) || (move > 0 && rb.velocity.x < 0)))
+            {
+                isSkid = true;
                 StartCoroutine("SkidToStop");
             }
             else
             {
-                rb.velocity = xMovement + new Vector3(0.0f, rb.velocity.y, 0.0f);//yMovement;
+                rb.velocity = xMovement + new Vector3(0.0f, rb.velocity.y, 0.0f);
             }
-        }
-
-        if (yMovement.y > 0.0f)
-        {
-            //rb.velocity = new Vector3(rb.velocity.x, 0.0f, 0.0f) + yMovement;
-        }
-
-        if (rb.velocity.y < 0.0f)
-        {
-            //rb.velocity -= Physics2D.gravity * fallMultiplier * Time.deltaTime;
         }
     }
 
@@ -269,24 +344,61 @@ public class Controller2D : MonoBehaviour
     {
         isSkid = true;
 
-        rb.velocity = new Vector2(rb.velocity.x * 0.75f, rb.velocity.y);
+        float startPos = transform.position.x;
+        float maxSkid = 3.0f;
+        int dir = 1;
 
-        yield return new WaitForSeconds(skidTime/4f);
+        if (rb.velocity.x > 0.0f) //find direction to skid
+        {
+            dir = 1;
+            facingRight = false; //face opposite of skid
+        }
+        else if (rb.velocity.x < 0)
+        {
+            dir = -1;
+            facingRight = true; //face opposite of skid
+        }
 
-        rb.velocity = new Vector2(rb.velocity.x * 0.75f, rb.velocity.y);
+        float endPos = transform.position.x + (maxSkid * dir);
 
-        yield return new WaitForSeconds(skidTime / 4f);
+        float i = 0;
 
-        rb.velocity = new Vector2(rb.velocity.x * 0.75f, rb.velocity.y);
+        if (endPos > startPos)
+        {
+            while (endPos > transform.position.x)
+            {
+                if (i > 25)
+                {
+                    break;
+                }
 
-        yield return new WaitForSeconds(skidTime / 4f);
+                i++;
+
+                rb.velocity = new Vector2(rb.velocity.x * Mathf.Pow(1f - decelerate * Time.deltaTime, 2f), rb.velocity.y);
+
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        else
+        {
+            while (endPos < transform.position.x)
+            {
+                if (i > 25)
+                {
+                    break;
+                }
+
+                i++;
+
+                rb.velocity = new Vector2(rb.velocity.x * Mathf.Pow(1f - decelerate * Time.deltaTime, 2f), rb.velocity.y);
+
+                yield return new WaitForFixedUpdate();
+            }
+        }
 
         rb.velocity = new Vector2(0, rb.velocity.y);
 
-        yield return new WaitForSeconds(skidTime / 4f);
-
         isSkid = false;
-
         yield break;
     }
 }

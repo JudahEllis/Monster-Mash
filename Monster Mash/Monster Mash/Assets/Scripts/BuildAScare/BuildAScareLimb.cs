b7bed495bfa0349be61eb3be24f05b0f7ce9e166
BarrettaJeremy10 <barrettajeremy10@gmail.com>
2024-03-26 18:40:20 -0700

    Added Basic Movement

---
+++ Monster Mash/Monster Mash/Assets/Scripts/BuildAScare/BuildAScareLimb.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BuildAScareLimb : MonoBehaviour
+{
+    bool isSelected = false;
+
+    PlayerInput input;
+
+    InputAction moveLimb;
+
+    CharacterController limbController;
+    
+    void Start()
+    {
+        SpawnObject();
+    }
+
+    public void SpawnObject()
+    {
+        isSelected = true;
+
+        input = FindObjectOfType<PlayerInput>();
+
+        moveLimb = input.actions.FindActionMap("Build-A-Scare").FindAction("Move Part");
+
+        limbController = GetComponent<CharacterController>();
+    }
+
+
+    void FixedUpdate()
+    {
+        MoveLimb();
+    }
+
+    void MoveLimb()
+    {
+        if(isSelected)
+        {
+
+            Vector3 xMovement = Camera.main.transform.right * moveLimb.ReadValue<Vector2>().x;
+
+            Vector3 yMovement = new Vector3(0, moveLimb.ReadValue<Vector2>().y, 0);
+
+            Vector3 movemnt = xMovement + yMovement;
+
+            //moveLimb.ReadValue<Vector2>()
+
+            limbController.Move(movemnt * Time.deltaTime * 2);
+        }
+    }
+}

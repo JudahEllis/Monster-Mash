29f7b3d35b28be3a24835aee63490e5fc3942272
HenryGossGrubbs <123278438+HenryGossGrubbs@users.noreply.github.com>
2024-06-18 15:22:39 -0700

    Began changing sfx manager to accept randomized sounds

--- Monster Mash/Monster Mash/Assets/Scripts/SFXManager.cs
+++ Monster Mash/Monster Mash/Assets/Scripts/SFXManager.cs
@@ -1,48 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameAudioScriptingEssentials;
 using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
-    [SerializeField] AudioSource[] clips;
+    [SerializeField] AudioClipRandomizer[] groundType;
+    [SerializeField] AudioClipRandomizer[] weight;
 
     public void footstepSFX(monsterPart part)
     {
         if (part.isLeg)
         {
-            Debug.Log("Play Footstep SFX");
             RaycastHit hit;
+            List<AudioClipRandomizer> toPlay = new List<AudioClipRandomizer>();
             if (Physics.Raycast(part.transform.position, Vector3.down, out hit))
             {
                 MeshFilter meshFilter = hit.collider.GetComponent<MeshFilter>();
                 string material = meshFilter.ToString();
                 if (material.Contains("table"))
                 {
-                    Debug.Log("wood");
-                    clips[3].Play();
+                    toPlay.Add(groundType[3]);
                 }
                 else if (material.Contains("pot"))
                 {
-                    Debug.Log("pot");
-                    clips[2].Play();
+                    toPlay.Add(groundType[2]);
                 }
                 else if (material.Contains("planter") || material.Contains("dirt"))
                 {
-                    Debug.Log("dirt");
-                    clips[0].Play();
+                    toPlay.Add(groundType[0]);
                 }
                 else if (material.Contains("can"))
                 {
-                    Debug.Log("metal");
-                    clips[1].Play();
+                    toPlay.Add(groundType[1]);
                 }
                 else
                 {
-                    clips[3].Play();
-                    Debug.Log("wood");
+                    toPlay.Add(groundType[3]);
                 }
-
             }
+            else
+            {
+                toPlay.Add(groundType[3]);
+            }
+
+            // adjust later when monster weight is added
+            toPlay.Add(weight[0]);
+
+            foreach (AudioClipRandomizer clip in toPlay)
+            {
+                clip.PlaySFX();
+            }
+
         }
     }
 }
